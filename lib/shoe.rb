81222bfedc1c279541ecdb7bb48dfc2be6ab4a13
cindych419@gmail.com <cindych419@gmail.com>
2018-05-09 22:39:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-
+BRANDS = []
 
   def initialize(brand)
-    BRAND = []
+    
     @brand = brand
     BRANDS << brand
   end
